14939e6f35863d9f6c5cf97e59407bdf6c8f9857
plukevdh <hi@plukevdh.me>
2023-01-26 01:00:36 -0500

    allow TF to output if GH actions requests debug logging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
 require 'rspec/terraform'
 
+# enable debug output if explicitly disabled or if GH actions requests it
+def debug?
+  ENV.fetch('SILENCE_TERRAFORM', "true") != "true" ||
+    ENV.fetch('ACTIONS_RUNNER_DEBUG', "false") == "true"
+end
+
 RSpec.configure do |config|
   config.terraform_log_level = :warn
-  config.terraform_stdout = Logger::LogDevice.new(IO::NULL) if ENV.fetch('SILENCE_TERRAFORM', "true") == "true"
+  config.terraform_stdout = Logger::LogDevice.new(IO::NULL) unless debug?
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
