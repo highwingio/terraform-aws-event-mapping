bab33938ddaf3cc0091690d6b43f88b4dbedc05d
plukevdh <hi@plukevdh.me>
2023-01-26 00:53:40 -0500

    silence TF output for CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'rspec/terraform'
 
 RSpec.configure do |config|
+  config.terraform_log_level = :warn
+  config.terraform_stdout = Logger::LogDevice.new(IO::NULL) if ENV.fetch('SILENCE_TERRAFORM', "true") == "true"
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
